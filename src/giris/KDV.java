b356d7bcb5a6c9edae0afc319b1632c009d757fb
omercoskun0 <omerrfcoskun0@gmail.com>
2023-12-18 15:23:30 +0300

    ....

---
+++ src/giris/KDV.java
@@ -0,0 +1,22 @@
+package giris;
+import java.util.Scanner;
+
+public class KDV {
+    public static void main(String[] args) {
+
+        double fiyat,kdvOran, KdvliFiyat, kdvTutar;
+
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Fiyat Giriniz: ");
+        fiyat = inp.nextDouble();
+        kdvOran = fiyat <= 1000 ? 0.18 : 0.8;
+        kdvTutar = fiyat * kdvOran ;
+        KdvliFiyat = fiyat + kdvTutar;
+
+
+        System.out.println("KDV'siz Tutar : " + fiyat);
+        System.out.println("KDV Oranı : " + kdvOran);
+        System.out.println("KDV tutarı: " + kdvTutar);
+        System.out.println("KDV'li tutar: " + KdvliFiyat);
+    }
+}
